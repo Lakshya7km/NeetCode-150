d666cf4191dbd412d6cf421a279298827bf1db72
Lakshya_k <lakshyakumar7cr@gmail.com>
2026-08-19 22:43:00 +0530

    Add: duplicate-integer - submission-12

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+      Arrays.sort(nums);
+
+           for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+           }
+           return false; 
+    //  HashSet<Integer> set =  new HashSet<>();
+
+    //  for(int i=0;i<nums.length;i++){
+    //   if(!set.contains(nums[i])){
+    //     set.add(nums[i]);
+    //   }else{
+    //     return true;
+    //   }
+    //  }
+    
+    // return false;
+      
+    }
+}
